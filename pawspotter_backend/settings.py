454225191ac27c8d01cebd811090829436985922
Sully <seam.sullyvan@gmail.com>
2025-03-08 11:47:16 +0800

    Fixed incorrect middleware causing authentication error

--- pawspotter_backend/settings.py
+++ pawspotter_backend/settings.py
@@ -66,7 +66,6 @@ MIDDLEWARE = [
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
-    "django.middleware.authentication.RemoteUserMiddleware",
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
